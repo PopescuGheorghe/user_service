59ee69fd0693f9298103df651b30de52cdc2d1e1
PopescuGheorghe <gimmy.xdd@gmail.com>
2015-11-28 19:48:27 +0200

    refactored users with index and show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,23 +4,27 @@ module Api
       before_filter :authenticate
 
       def index
-       render json: build_data_object(User.all), status: 200
+       respond_with build_data_object(User.all)
+      end
+
+      def show
+        respond_with build_data_object(User.find(params[:id]))
       end
 
       def create
         user = User.new(user_params)
         if user.save
-          render json: build_data_object(user), status: 200
+          render json: build_data_object(user), status: 201
         else
           render json: build_error_object(user), status: 403
         end
       end
 
-    private
+      private
 
-    def user_params
-      params.require(:user).permit(:email, :password)
-    end
+      def user_params
+        params.require(:user).permit(:email, :password)
+      end
 
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1, path: '/' do
       resources :sessions,  :only => [:create, :destroy]
-      resources :users
+      resources :users,     :only => [:index, :show, :create]
     end
   end
 end
