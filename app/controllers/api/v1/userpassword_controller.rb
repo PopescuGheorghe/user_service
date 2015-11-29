5cefb630122e513c5c9ed333cd0b2f475d1059f1
PopescuGheorghe <gimmy.xdd@gmail.com>
2015-11-29 02:48:28 +0200

    finishe update password prototype

--- app/controllers/api/v1/userpassword_controller.rb
+++ app/controllers/api/v1/userpassword_controller.rb
@@ -1,19 +1,17 @@
 class Api::V1::UserpasswordController < Api::BaseController
 
   def update
-    @user = User.find_by(id: params[:id])
-
-    # ToDO check for password match
-
-    if @user.update(user_params)
-      render json: build_data_object(@user), status: 200
+    user = User.find_by(id: params[:id])
+    if user.update_with_password(user_params)
+      render json: build_data_object(user), status: 200
+    else
+      render json: build_error_object(user), status: 403
     end
   end
 
   private
 
   def user_params
-    # NOTE: Using `strong_parameters` gem
-    params.require(:user).permit(:password)
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
   end
 end
