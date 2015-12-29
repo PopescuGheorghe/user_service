4b39592f86630937d93c3e575573f032fab0b1d3
PopescuGheorghe <gimmy.xdd@gmail.com>
2015-12-29 15:18:12 +0200

    updated cors

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,10 @@ module UserService
     # Enable CORS
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
-        origins '*'
+        origins '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
 
         resource '/cors',
           :headers => :any,
